2f92b437b3dd9df50a8071f27bc8600b82d6cbb3
Giacomo Pedemonte <giacomopedemonte@libero.it>
2025-06-10 18:56:44 +0200

    fix: update import paths for gradient_button and auth_repository

--- integration_test/gradient_button_integration_test.dart
+++ integration_test/gradient_button_integration_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:isyfit/widgets/gradient_button.dart';
+import 'package:isyfit/presentation/widgets/gradient_button.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
